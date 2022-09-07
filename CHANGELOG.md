46d7fa67c4eb90c829dcebb9f86ef51d620328df
Andreas Sedlmayer <sedlmayer.andreas89@gmail.com>
2022-09-07 18:37:35 +0200

    updated mmconnect config settings

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## v1.4.4
+- Update Nightscout from 14.2.2 to 14.2.5
+- Update base images
+- Update dependencies
+- Remove unsupported architectures (armv7 and i386)
+- added mmconnect env variables to config yaml
+
 ## v1.2.0
 - Update Nightscout from 14.0.4 to 14.2.2
 - Update base images
--- nightscout/rootfs/etc/cont-init.d/nightscout.sh
+++ nightscout/rootfs/etc/cont-init.d/nightscout.sh
@@ -8,3 +8,26 @@ readonly api_key=$(bashio::config 'api_key')
 
 bashio::log.info "Setting up API KEY: ${api_key}"
 export API_SECRET="${api_key}"
+
+#mmconnect
+if bashio::config.has_value 'mmconnect_user_name'; then
+    readonly mmconnect_user_name=$(bashio::config 'mmconnect_user_name')
+    bashio::log.debug "Setting up MMCONNECT_USER_NAME: ${mmconnect_user_name}"
+    export MMCONNECT_USER_NAME="${mmconnect_user_name}"
+fi
+if bashio::config.has_value 'mmconnect_password'; then
+    readonly mmconnect_password=$(bashio::config 'mmconnect_password')
+    bashio::log.debug "Setting up MMCONNECT_PASSWORD: ${mmconnect_password}"
+    export MMCONNECT_PASSWORD="${mmconnect_password}"
+fi
+if bashio::config.has_value 'mmconnect_verbose'; then
+    readonly mmconnect_verbose=$(bashio::config 'mmconnect_verbose')
+    bashio::log.debug "Setting up MMCONNECT_VERBOSE: ${mmconnect_verbose}"
+    export MMCONNECT_VERBOSE="${mmconnect_verbose}"
+fi
+if bashio::config.has_value 'mmconnect_server'; then
+    readonly mmconnect_server=$(bashio::config 'mmconnect_server')
+    bashio::log.debug "Setting up MMCONNECT_SERVER: ${mmconnect_server}"
+    export MMCONNECT_SERVER="${mmconnect_server}"
+fi
+
