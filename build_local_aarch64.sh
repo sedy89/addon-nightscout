609d8c83718e6a596bab893d5ac77a3427eff87a
Andreas Sedlmayer <sedlmayer.andreas89@gmail.com>
2022-09-08 16:17:17 +0200

    patched transform.js to fix timestamp issue from mmconnect

--- build_local_aarch64.sh
+++ build_local_aarch64.sh
@@ -1,1 +1,1 @@
-cd nightscout && docker build --build-arg BUILD_VERSION=1.4.8 --build-arg BUILD_FROM="ghcr.io/hassio-addons/ubuntu-base/aarch64:7.2.0" --build-arg BUILD_ARCH=aarch64 --progress plain --no-cache -t nightscout .
+cd nightscout && docker build --build-arg BUILD_VERSION=1.4.9 --build-arg BUILD_FROM="ghcr.io/hassio-addons/ubuntu-base/aarch64:7.2.0" --build-arg BUILD_ARCH=aarch64 --progress plain --no-cache -t nightscout .
